a36528dc26acc32f6b05f57548f1905df4ca2122
Armando Flores Montenegro <afloresmontenegro.1@gmail.com>
2026-08-21 15:39:12 -0400

    M2: dolphin/os/OSCache.c matched (8 funcs, 292B, fuzzy 100%) — 5/5 SDK files, 22/23 funcs

--- configure.py
+++ configure.py
@@ -290,6 +290,7 @@ config.libs = [
         Object(Matching, "dolphin/os/OSTime.c"),
         Object(Matching, "dolphin/os/OSClearStack.c"),
         Object(Matching, "dolphin/os/OSArena.c"),
+        Object(Matching, "dolphin/os/OSCache.c"),
     ]),
     {
         "lib": "Runtime.PPCEABI.H",
---
+++ src/dolphin/os/OSCache.c
@@ -0,0 +1,119 @@
+typedef unsigned int u32;
+
+asm void DCEnable(void)
+{
+    nofralloc
+    sync
+    mfspr    r3, 1008
+    ori      r3, r3, 0x4000
+    mtspr    1008, r3
+    blr
+}
+
+asm void DCInvalidateRange(register void *addr, register u32 nBytes)
+{
+    nofralloc
+    cmplwi   r4, 0
+    blelr
+    clrlwi   r5, r3, 27
+    add      r4, r4, r5
+    addi     r4, r4, 0x1f
+    srwi     r4, r4, 5
+    mtctr    r4
+L8000B644:
+    dcbi     r0, r3
+    addi     r3, r3, 0x20
+    bdnz     L8000B644
+    blr
+}
+
+asm void DCFlushRange(register void *addr, register u32 nBytes)
+{
+    nofralloc
+    cmplwi   r4, 0
+    blelr
+    clrlwi   r5, r3, 27
+    add      r4, r4, r5
+    addi     r4, r4, 0x1f
+    srwi     r4, r4, 5
+    mtctr    r4
+L8000B670:
+    dcbf     r0, r3
+    addi     r3, r3, 0x20
+    bdnz     L8000B670
+    sc
+    blr
+}
+
+asm void DCStoreRange(register void *addr, register u32 nBytes)
+{
+    nofralloc
+    cmplwi   r4, 0
+    blelr
+    clrlwi   r5, r3, 27
+    add      r4, r4, r5
+    addi     r4, r4, 0x1f
+    srwi     r4, r4, 5
+    mtctr    r4
+L8000B6A0:
+    dcbst    r0, r3
+    addi     r3, r3, 0x20
+    bdnz     L8000B6A0
+    sc
+    blr
+}
+
+asm void DCFlushRangeNoSync(register void *addr, register u32 nBytes)
+{
+    nofralloc
+    cmplwi   r4, 0
+    blelr
+    clrlwi   r5, r3, 27
+    add      r4, r4, r5
+    addi     r4, r4, 0x1f
+    srwi     r4, r4, 5
+    mtctr    r4
+L8000B6D0:
+    dcbf     r0, r3
+    addi     r3, r3, 0x20
+    bdnz     L8000B6D0
+    blr
+}
+
+asm void ICInvalidateRange(register void *addr, register u32 nBytes)
+{
+    nofralloc
+    cmplwi   r4, 0
+    blelr
+    clrlwi   r5, r3, 27
+    add      r4, r4, r5
+    addi     r4, r4, 0x1f
+    srwi     r4, r4, 5
+    mtctr    r4
+L8000B6FC:
+    icbi     r0, r3
+    addi     r3, r3, 0x20
+    bdnz     L8000B6FC
+    sync
+    isync
+    blr
+}
+
+asm void ICFlashInvalidate(void)
+{
+    nofralloc
+    mfspr    r3, 1008
+    ori      r3, r3, 0x800
+    mtspr    1008, r3
+    blr
+}
+
+asm void ICEnable(void)
+{
+    nofralloc
+    isync
+    mfspr    r3, 1008
+    ori      r3, r3, 0x8000
+    mtspr    1008, r3
+    blr
+}
