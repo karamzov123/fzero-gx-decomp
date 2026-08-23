7d4ab11570d5c38317d614df06053ab80442e36f
Armando Flores Montenegro <afloresmontenegro.1@gmail.com>
2026-08-23 00:06:59 -0400

    carve main.c ext [8A4C-8DB4) fn_80008A4C/8BA8/8BEC/8C20 872B asm transcription, coarse->text_80008DB4 (OS heap family only remains), gate sha1 green

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@ extern void DCStoreRange(void);
 extern void DVDGetDriveStatus(void);
 extern void DVDInit(void);
 extern void GXBegin(void);
+extern void OSAllocFromHeap(void);
 extern void OSGetArenaHi(void);
 extern void OSGetArenaLo(void);
 extern void OSGetProgressiveMode(void);
@@ -88,12 +89,16 @@ extern void fn_80008204(void);
 extern void fn_800084E8(void);
 extern void fn_800087F4(void);
 extern void fn_80008A4C(void);
+extern void fn_80008BA8(void);
+extern void fn_80008BEC(void);
+extern void fn_80008C20(void);
 extern void fn_8000B334(void);
 extern void fn_8000B360(void);
 extern void fn_8000C49C(void);
 extern void fn_8000CDD8(void);
 extern void fn_8000CEBC(void);
 extern void fn_8000D1F0(void);
+extern void fn_80015B78(void);
 extern void fn_80015EE8(void);
 extern void fn_80016DF8(void);
 extern void fn_800170EC(void);
@@ -123,6 +128,8 @@ extern void fn_8006CD40(void);
 extern void fn_8006CDFC(void);
 extern void fn_8006CE1C(void);
 extern void fn_8006CFF8(void);
+extern void fn_8006E5FC(void);
+extern void fn_8006EB4C(void);
 extern void fn_8006FCB4(void);
 extern void fn_8006FD1C(void);
 extern void fn_8006FDEC(void);
@@ -165,11 +172,14 @@ extern void fn_80073C6C(void);
 extern void fn_800744F8(void);
 extern void fn_800745A4(void);
 extern void fn_80074660(void);
+extern void fn_800746A8(void);
 extern void fn_80074788(void);
 extern void fn_800747D0(void);
 extern void fn_80074918(void);
 extern void fn_800791A4(void);
+extern void fn_800791E8(void);
 extern void fn_800793D4(void);
+extern void fn_800794F0(void);
 extern void fn_80079764(void);
 extern void fn_80083B8C(void);
 extern void fn_80083BCC(void);
@@ -177,9 +187,12 @@ extern void fn_80083D40(void);
 extern void fn_80083D6C(void);
 extern void fn_80083DB0(void);
 extern void lbl_8006D188(void);
+extern void lbl_8006D668(void);
 extern void lbl_8006D758(void);
+extern void lbl_8006D7DC(void);
 extern void lbl_8006DAEC(void);
 extern void lbl_8006DB30(void);
+extern void lbl_8006DBAC(void);
 extern void main(void);
 extern void memcpy(void);
 extern void memset(void);
@@ -4038,6 +4051,253 @@ _80008a30:
     lwz	r31, 0x1c(r1)
     mtlr	r0
     addi	r1, r1, 0x20
+    blr	
+}
+
+asm void fn_80008A4C(void)
+{
+    nofralloc
+    stwu	r1, -0x20(r1)
+    mflr	r0
+    li	r4, 1
+    li	r5, 0
+    stw	r0, 0x24(r1)
+    li	r6, 0
+    stw	r31, 0x1c(r1)
+    mr	r31, r3
+    li	r3, 0
+    bl      fn_800728A8
+    li	r3, 1
+    li	r4, 3
+    li	r5, 1
+    bl      fn_80074918
+    li	r3, 0
+    bl      fn_800720B0
+    li	r3, 6
+    li	r4, 0
+    bl      fn_800746A8
+    li	r3, 0
+    bl      fn_80072864
+    li	r3, 0
+    bl      fn_80073C6C
+    li	r3, 0
+    li	r4, 1
+    li	r5, 4
+    li	r6, 0x3c
+    li	r7, 0
+    li	r8, 0x7d
+    bl      fn_800745A4
+    li	r3, 0
+    li	r4, 0
+    li	r5, 0
+    li	r6, 0xff
+    bl      fn_800734A8
+    lwz	r0, 0x18(r31)
+    addi	r4, r1, 8
+    li	r3, 0
+    stw	r0, 8(r1)
+    bl      fn_800736C0
+    li	r3, 0
+    li	r4, 0xc
+    bl      fn_800735C8
+    li	r3, 0
+    li	r4, 0x1c
+    bl      fn_80073620
+    li	r3, 0
+    li	r4, 0xf
+    li	r5, 0xf
+    li	r6, 0xf
+    li	r7, 0xe
+    bl      fn_80072C24
+    li	r3, 0
+    li	r4, 0
+    li	r5, 0
+    li	r6, 0
+    li	r7, 1
+    li	r8, 0
+    bl      fn_80072D64
+    li	r3, 0
+    li	r4, 7
+    li	r5, 7
+    li	r6, 7
+    li	r7, 6
+    bl      fn_80072CC4
+    li	r3, 0
+    li	r4, 0
+    li	r5, 0
+    li	r6, 0
+    li	r7, 1
+    li	r8, 0
+    bl      fn_80072E20
+    li	r3, 1
+    bl      fn_80073678
+    li	r3, 1
+    bl      fn_80074660
+    li	r3, 0
+    bl      fn_80073898
+    li	r3, 0
+    bl      fn_80074788
+    li	r3, 0x200
+    bl      fn_8007245C
+    lwz	r0, 0x24(r1)
+    lwz	r31, 0x1c(r1)
+    mtlr	r0
+    addi	r1, r1, 0x20
+    blr	
+}
+
+asm void fn_80008BA8(void)
+{
+    nofralloc
+    stwu	r1, -0x10(r1)
+    mflr	r0
+    or	r6, r4, r3
+    stw	r0, 0x14(r1)
+    clrlwi.	r0, r6, 0x1e
+    bc      12, 2, _80008bc8
+    bl      memcpy
+    b       _80008bdc
+_80008bc8:
+    clrlwi.	r0, r6, 0x1b
+    bc      12, 2, _80008bd8
+    bl      fn_800794F0
+    b       _80008bdc
+_80008bd8:
+    bl      fn_800791E8
+_80008bdc:
+    lwz	r0, 0x14(r1)
+    mtlr	r0
+    addi	r1, r1, 0x10
+    blr	
+}
+
+asm void fn_80008BEC(void)
+{
+    nofralloc
+    stwu	r1, -0x10(r1)
+    mflr	r0
+    stw	r0, 0x14(r1)
+    clrlwi.	r0, r3, 0x1b
+    bc      4, 2, _80008c08
+    bl      fn_800793D4
+    b       _80008c10
+_80008c08:
+    clrlwi	r4, r4, 0x18
+    bl      memset
+_80008c10:
+    lwz	r0, 0x14(r1)
+    mtlr	r0
+    addi	r1, r1, 0x10
+    blr	
+}
+
+asm void fn_80008C20(void)
+{
+    nofralloc
+    stwu	r1, -0x40(r1)
+    mflr	r0
+    stw	r0, 0x44(r1)
+    stw	r31, 0x3c(r1)
+    mr	r31, r4
+    stw	r30, 0x38(r1)
+    mr	r30, r3
+    addi	r3, r1, 8
+    lfs	f0, 0(r5)
+    fneg	f0, f0
+    stfs	f0, 8(r1)
+    lfs	f0, 4(r5)
+    fneg	f0, f0
+    stfs	f0, 0xc(r1)
+    lfs	f0, 8(r5)
+    fneg	f0, f0
+    stfs	f0, 0x10(r1)
+    bl      lbl_8006D668
+    lfs	f0, -0x7f10(r2)
+    fcmpu	cr0, f0, f1
+    bc      4, 2, _80008c80
+    mr	r3, r30
+    bl      lbl_8006D7DC
+    b       _80008d9c
+_80008c80:
+    lfs	f6, 4(r31)
+    addi	r3, r1, 0x20
+    lfs	f3, 0x10(r1)
+    lfs	f5, 8(r31)
+    lfs	f4, 8(r1)
+    fmuls	f2, f6, f3
+    lfs	f0, 0xc(r1)
+    lfs	f7, 0(r31)
+    fmuls	f1, f5, f4
+    fnmsubs	f2, f5, f0, f2
+    fmuls	f0, f7, f0
+    fnmsubs	f1, f7, f3, f1
+    stfs	f2, 0x20(r1)
+    fnmsubs	f0, f6, f4, f0
+    stfs	f1, 0x24(r1)
+    stfs	f0, 0x28(r1)
+    bl      lbl_8006D668
+    lfs	f0, -0x7f10(r2)
+    fcmpu	cr0, f0, f1
+    bc      4, 2, _80008cdc
+    mr	r3, r30
+    bl      lbl_8006D7DC
+    b       _80008d9c
+_80008cdc:
+    lfs	f6, 0xc(r1)
+    addi	r3, r1, 0x14
+    lfs	f3, 0x28(r1)
+    lfs	f5, 0x10(r1)
+    lfs	f4, 0x20(r1)
+    fmuls	f2, f6, f3
+    lfs	f0, 0x24(r1)
+    lfs	f7, 8(r1)
+    fmuls	f1, f5, f4
+    fnmsubs	f2, f5, f0, f2
+    fmuls	f0, f7, f0
+    fnmsubs	f1, f7, f3, f1
+    stfs	f2, 0x14(r1)
+    fnmsubs	f0, f6, f4, f0
+    stfs	f1, 0x18(r1)
+    stfs	f0, 0x1c(r1)
+    bl      lbl_8006D668
+    lfs	f0, -0x7f10(r2)
+    fcmpu	cr0, f0, f1
+    bc      4, 2, _80008d38
+    mr	r3, r30
+    bl      lbl_8006D7DC
+    b       _80008d9c
+_80008d38:
+    lwz	r3, -0x76c0(r13)
+    lfs	f0, 0x20(r1)
+    stfs	f0, 0(r3)
+    lfs	f0, 0x14(r1)
+    stfs	f0, 4(r3)
+    lfs	f0, 8(r1)
+    stfs	f0, 8(r3)
+    lfs	f0, 0(r30)
+    stfs	f0, 0xc(r3)
+    lfs	f0, 0x24(r1)
+    stfs	f0, 0x10(r3)
+    lfs	f0, 0x18(r1)
+    stfs	f0, 0x14(r3)
+    lfs	f0, 0xc(r1)
+    stfs	f0, 0x18(r3)
+    lfs	f0, 4(r30)
+    stfs	f0, 0x1c(r3)
+    lfs	f0, 0x28(r1)
+    stfs	f0, 0x20(r3)
+    lfs	f0, 0x1c(r1)
+    stfs	f0, 0x24(r3)
+    lfs	f0, 0x10(r1)
+    stfs	f0, 0x28(r3)
+    lfs	f0, 8(r30)
+    stfs	f0, 0x2c(r3)
+_80008d9c:
+    lwz	r0, 0x44(r1)
+    lwz	r31, 0x3c(r1)
+    lwz	r30, 0x38(r1)
+    mtlr	r0
+    addi	r1, r1, 0x40
     blr	
 }
 
