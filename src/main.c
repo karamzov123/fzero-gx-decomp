d9fcdea3bf56188b5faf1a14bd5f851af8d9b365
Armando Flores Montenegro <afloresmontenegro.1@gmail.com>
2026-08-23 00:02:11 -0400

    carve main.c ext [7654-79A0) fn_80007654/7664/7700/7730/77E0 844B asm transcription (tlbie/tlbsync), coarse->text_800079A0, gate sha1 green

--- src/main.c
+++ src/main.c
@@ -72,6 +72,9 @@ extern void fn_80007664(void);
 extern void fn_80007700(void);
 extern void fn_80007730(void);
 extern void fn_800077E0(void);
+extern void fn_800079A0(void);
+extern void fn_800079C4(void);
+extern void fn_80007A00(void);
 extern void fn_80007A44(void);
 extern void fn_80007B68(void);
 extern void fn_8000B334(void);
@@ -2581,6 +2584,253 @@ _80007624:
 _8000762c:
     cmplw	r29, r28
     bc      12, 0, _80007600
+    lwz	r0, 0x24(r1)
+    lwz	r31, 0x1c(r1)
+    lwz	r30, 0x18(r1)
+    lwz	r29, 0x14(r1)
+    lwz	r28, 0x10(r1)
+    mtlr	r0
+    addi	r1, r1, 0x20
+    blr	
+}
+
+asm void fn_80007654(void)
+{
+    nofralloc
+    rlwinm	r3, r3, 0, 0, 3
+    mfsrin	r3, r3
+    clrlwi	r3, r3, 8
+    blr	
+}
+
+asm void fn_80007664(void)
+{
+    nofralloc
+    lbz	r0, 6(r3)
+    li	r6, 0
+    rlwimi	r0, r6, 1, 0x1c, 0x1e
+    rlwinm	r5, r5, 0x14, 0x10, 0x1f
+    stb	r0, 6(r3)
+    lbz	r0, 7(r3)
+    rlwimi	r0, r6, 2, 0x1d, 0x1d
+    stb	r0, 7(r3)
+    lwz	r0, 4(r3)
+    rlwimi	r0, r5, 0xc, 0, 0x13
+    stw	r0, 4(r3)
+    lbz	r0, 6(r3)
+    rlwimi	r0, r6, 0, 0x1f, 0x1f
+    stb	r0, 6(r3)
+    lbz	r0, 7(r3)
+    rlwimi	r0, r6, 7, 0x18, 0x18
+    stb	r0, 7(r3)
+    lbz	r0, 7(r3)
+    rlwimi	r0, r6, 4, 0x1b, 0x1b
+    stb	r0, 7(r3)
+    lbz	r0, 7(r3)
+    rlwimi	r0, r6, 3, 0x1c, 0x1c
+    stb	r0, 7(r3)
+    sync	
+    tlbie	r4
+    sync	
+    tlbsync	
+    sync	
+    lbz	r0, 0(r3)
+    li	r5, 1
+    rlwimi	r0, r5, 7, 0x18, 0x18
+    stb	r0, 0(r3)
+    dcbf	0, r3
+    sync	
+    tlbie	r4
+    sync	
+    tlbsync	
+    sync	
+    blr	
+}
+
+asm void fn_80007700(void)
+{
+    nofralloc
+    sync	
+    tlbie	r4
+    sync	
+    tlbsync	
+    sync	
+    dcbf	0, r3
+    sync	
+    tlbie	r4
+    sync	
+    tlbsync	
+    sync	
+    blr	
+}
+
+asm void fn_80007730(void)
+{
+    nofralloc
+    stwu	r1, -0x20(r1)
+    mflr	r0
+    stw	r0, 0x24(r1)
+    stw	r31, 0x1c(r1)
+    stw	r30, 0x18(r1)
+    stw	r29, 0x14(r1)
+    mr	r29, r3
+    bl      fn_800079C4
+    stw	r3, 8(r1)
+    mr	r3, r29
+    bl      fn_80007A00
+    stw	r3, 0xc(r1)
+    addi	r30, r1, 8
+    li	r29, 0
+    li	r31, 8
+_8000776c:
+    lwz	r3, 0(r30)
+    bl      fn_800079A0
+    li	r5, 0
+    mr	r4, r3
+    mtctr	r31
+_80007780:
+    lbz	r0, 0(r4)
+    rlwinm.	r0, r0, 0x19, 0x1f, 0x1f
+    bc      4, 2, _800077a4
+    slwi	r0, r5, 3
+    add	r3, r3, r0
+    lbz	r0, 3(r3)
+    rlwimi	r0, r29, 6, 0x19, 0x19
+    stb	r0, 3(r3)
+    b       _800077c4
+_800077a4:
+    addi	r4, r4, 8
+    addi	r5, r5, 1
+    bc      16, 0, _80007780
+    addi	r29, r29, 1
+    addi	r30, r30, 4
+    cmplwi	r29, 2
+    bc      12, 0, _8000776c
+    li	r3, 0
+_800077c4:
+    lwz	r0, 0x24(r1)
+    lwz	r31, 0x1c(r1)
+    lwz	r30, 0x18(r1)
+    lwz	r29, 0x14(r1)
+    mtlr	r0
+    addi	r1, r1, 0x20
+    blr	
+}
+
+asm void fn_800077E0(void)
+{
+    nofralloc
+    stwu	r1, -0x20(r1)
+    mflr	r0
+    stw	r0, 0x24(r1)
+    stw	r31, 0x1c(r1)
+    stw	r30, 0x18(r1)
+    stw	r29, 0x14(r1)
+    stw	r28, 0x10(r1)
+    mr	r28, r3
+    bl      fn_80007654
+    mr	r31, r3
+    mr	r3, r28
+    rlwinm	r29, r28, 0xa, 0x1a, 0x1f
+    bl      fn_800079C4
+    stw	r3, 8(r1)
+    mr	r3, r28
+    bl      fn_80007A00
+    stw	r3, 0xc(r1)
+    addi	r30, r1, 8
+    li	r28, 0
+_8000782c:
+    lwz	r3, 0(r30)
+    bl      fn_800079A0
+    li	r0, 2
+    li	r6, 0
+    mr	r5, r3
+    mtctr	r0
+_80007844:
+    lbz	r0, 0(r5)
+    rlwinm.	r0, r0, 0x19, 0x1f, 0x1f
+    bc      12, 2, _80007888
+    lbz	r4, 3(r5)
+    rlwinm	r0, r4, 0x1a, 0x1f, 0x1f
+    cmplw	r28, r0
+    bc      4, 2, _80007888
+    clrlwi	r0, r4, 0x1a
+    cmplw	r29, r0
+    bc      4, 2, _80007888
+    lwz	r0, 0(r5)
+    rlwinm	r0, r0, 0x19, 8, 0x1f
+    cmplw	r31, r0
+    bc      4, 2, _80007888
+    slwi	r0, r6, 3
+    add	r3, r3, r0
+    b       _80007980
+_80007888:
+    lbz	r0, 8(r5)
+    addi	r6, r6, 1
+    rlwinm.	r0, r0, 0x19, 0x1f, 0x1f
+    bc      12, 2, _800078d0
+    lbz	r4, 0xb(r5)
+    rlwinm	r0, r4, 0x1a, 0x1f, 0x1f
+    cmplw	r28, r0
+    bc      4, 2, _800078d0
+    clrlwi	r0, r4, 0x1a
+    cmplw	r29, r0
+    bc      4, 2, _800078d0
+    lwz	r0, 8(r5)
+    rlwinm	r0, r0, 0x19, 8, 0x1f
+    cmplw	r31, r0
+    bc      4, 2, _800078d0
+    slwi	r0, r6, 3
+    add	r3, r3, r0
+    b       _80007980
+_800078d0:
+    lbz	r0, 0x10(r5)
+    addi	r6, r6, 1
+    rlwinm.	r0, r0, 0x19, 0x1f, 0x1f
+    bc      12, 2, _80007918
+    lbz	r4, 0x13(r5)
+    rlwinm	r0, r4, 0x1a, 0x1f, 0x1f
+    cmplw	r28, r0
+    bc      4, 2, _80007918
+    clrlwi	r0, r4, 0x1a
+    cmplw	r29, r0
+    bc      4, 2, _80007918
+    lwz	r0, 0x10(r5)
+    rlwinm	r0, r0, 0x19, 8, 0x1f
+    cmplw	r31, r0
+    bc      4, 2, _80007918
+    slwi	r0, r6, 3
+    add	r3, r3, r0
+    b       _80007980
+_80007918:
+    lbz	r0, 0x18(r5)
+    addi	r6, r6, 1
+    rlwinm.	r0, r0, 0x19, 0x1f, 0x1f
+    bc      12, 2, _80007960
+    lbz	r4, 0x1b(r5)
+    rlwinm	r0, r4, 0x1a, 0x1f, 0x1f
+    cmplw	r28, r0
+    bc      4, 2, _80007960
+    clrlwi	r0, r4, 0x1a
+    cmplw	r29, r0
+    bc      4, 2, _80007960
+    lwz	r0, 0x18(r5)
+    rlwinm	r0, r0, 0x19, 8, 0x1f
+    cmplw	r31, r0
+    bc      4, 2, _80007960
+    slwi	r0, r6, 3
+    add	r3, r3, r0
+    b       _80007980
+_80007960:
+    addi	r5, r5, 0x20
+    addi	r6, r6, 1
+    bc      16, 0, _80007844
+    addi	r28, r28, 1
+    addi	r30, r30, 4
+    cmplwi	r28, 2
+    bc      12, 0, _8000782c
+    li	r3, 0
+_80007980:
     lwz	r0, 0x24(r1)
     lwz	r31, 0x1c(r1)
     lwz	r30, 0x18(r1)
