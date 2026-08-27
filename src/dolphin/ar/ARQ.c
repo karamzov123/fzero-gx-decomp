51ee56295960ffb6c6288e6d1169f503e3cdc1ec
karamzov123 <karamzov123@users.noreply.github.com>
2026-08-27 17:06:47 +0000

    public mirror: natc: land exact ARQSetChunkSize
    Filtered public export; source commit: a8dddfae4d43a7c05e795c0efaf91243daab58a9
    Generated docs/symbols.md from config/GFZE01/symbols.txt.

--- src/dolphin/ar/ARQ.c
+++ src/dolphin/ar/ARQ.c
@@ -77,7 +77,7 @@ void __ARQInterruptServiceRoutine(void) {
         lbl_801A6A14 = 0;
         lbl_801A6A1C = 0;
     }
-    if (lbl_801A6A00 != 0) {
+    if (lbl_801A6A00) {
         if (lbl_801A6A00->type == 0) {
             ARStartDMA(lbl_801A6A00->type, lbl_801A6A00->source, lbl_801A6A00->dest, lbl_801A6A00->length);
         } else {
@@ -108,123 +108,66 @@ void ARQInit(void) {
     }
 }
 
-asm void ARQPostRequest(register struct ARQRequest* request, register u32 owner, register u32 type, register u32 priority, register u32 source, register u32 dest, register u32 length, register ARQCallback callback)
-{
-    nofralloc
-    mflr	r0
-    cmplwi	r10, 0
-    stw	r0, 4(r1)
-    li	r0, 0
-    stwu	r1, -0x38(r1)
-    stw	r31, 0x34(r1)
-    stw	r30, 0x30(r1)
-    addi	r30, r6, 0
-    stw	r29, 0x2c(r1)
-    addi	r29, r3, 0
-    stw	r0, 0(r3)
-    stw	r4, 4(r3)
-    stw	r5, 8(r3)
-    stw	r7, 0x10(r3)
-    stw	r8, 0x14(r3)
-    stw	r9, 0x18(r3)
-    beq     _800205ec
-    stw	r10, 0x1c(r29)
-    b       _800205f8
-_800205ec:
-    lis     r3, __ARQCallbackHack@ha
-    addi	r0, r3, __ARQCallbackHack@l
-    stw	r0, 0x1c(r29)
-_800205f8:
-    bl      OSDisableInterrupts
-    cmpwi	r30, 1
-    addi	r31, r3, 0
-    beq     _8002063c
-    bge     _8002065c
-    cmpwi	r30, 0
-    bge     _80020618
-    b       _8002065c
-_80020618:
-    lwz	r0, lbl_801A6A08
-    cmplwi	r0, 0
-    beq     _80020630
-    lwz	r3, lbl_801A6A0C
-    stw	r29, 0(r3)
-    b       _80020634
-_80020630:
-    stw	r29, lbl_801A6A08
-_80020634:
-    stw	r29, lbl_801A6A0C
-    b       _8002065c
-_8002063c:
-    lwz	r0, lbl_801A6A00
-    cmplwi	r0, 0
-    beq     _80020654
-    lwz	r3, lbl_801A6A04
-    stw	r29, 0(r3)
-    b       _80020658
-_80020654:
-    stw	r29, lbl_801A6A00
-_80020658:
-    stw	r29, lbl_801A6A04
-_8002065c:
-    lwz	r0, lbl_801A6A10
-    cmplwi	r0, 0
-    bne     _800206d8
-    lwz	r0, lbl_801A6A14
-    cmplwi	r0, 0
-    bne     _800206d8
-    lwz	r6, lbl_801A6A00
-    cmplwi	r6, 0
-    beq     _800206c8
-    lwz	r3, 8(r6)
-    cmplwi	r3, 0
-    bne     _800206a0
-    lwz	r4, 0x10(r6)
-    lwz	r5, 0x14(r6)
-    lwz	r6, 0x18(r6)
-    bl      ARStartDMA
-    b       _800206b0
-_800206a0:
-    lwz	r4, 0x14(r6)
-    lwz	r5, 0x10(r6)
-    lwz	r6, 0x18(r6)
-    bl      ARStartDMA
-_800206b0:
-    lwz	r3, lbl_801A6A00
-    lwz	r0, 0x1c(r3)
-    stw	r0, lbl_801A6A18
-    stw	r3, lbl_801A6A10
-    lwz	r0, 0(r3)
-    stw	r0, lbl_801A6A00
-_800206c8:
-    lwz	r0, lbl_801A6A10
-    cmplwi	r0, 0
-    bne     _800206d8
-    bl      __ARQServiceQueueLo
-_800206d8:
-    mr	r3, r31
-    bl      OSRestoreInterrupts
-    lwz	r0, 0x3c(r1)
-    lwz	r31, 0x34(r1)
-    lwz	r30, 0x30(r1)
-    lwz	r29, 0x2c(r1)
-    addi	r1, r1, 0x38
-    mtlr	r0
-    blr	
+// provenance: dolsdk2001:src/ar/arq.c:108 (adapted; asserts absent in retail)
+void ARQPostRequest(register ARQReq* request, register u32 owner, register u32 type, register u32 priority,
+                    register u32 source, register u32 dest, register u32 length, register void (*callback)(u32)) {
+    int level;
+    request->next = 0;
+    request->owner = owner;
+    request->type = type;
+    request->source = source;
+    request->dest = dest;
+    request->length = length;
+    if (callback) {
+        request->callback = callback;
+    } else {
+        request->callback = __ARQCallbackHack;
+    }
+    level = OSDisableInterrupts();
+    switch (priority) {
+        case 0:
+            if (lbl_801A6A08) {
+                lbl_801A6A0C->next = request;
+            } else {
+                lbl_801A6A08 = request;
+            }
+            lbl_801A6A0C = request;
+            break;
+        case 1:
+            if (lbl_801A6A00) {
+                lbl_801A6A04->next = request;
+            } else {
+                lbl_801A6A00 = request;
+            }
+            lbl_801A6A04 = request;
+            break;
+    }
+    if (lbl_801A6A10 == 0 && lbl_801A6A14 == 0) {
+        if (lbl_801A6A00) {
+            if (lbl_801A6A00->type == 0) {
+                ARStartDMA(lbl_801A6A00->type, lbl_801A6A00->source, lbl_801A6A00->dest, lbl_801A6A00->length);
+            } else {
+                ARStartDMA(lbl_801A6A00->type, lbl_801A6A00->dest, lbl_801A6A00->source, lbl_801A6A00->length);
+            }
+            lbl_801A6A18 = lbl_801A6A00->callback;
+            lbl_801A6A10 = lbl_801A6A00;
+            lbl_801A6A00 = lbl_801A6A00->next;
+        }
+        if (lbl_801A6A10 == 0) {
+            __ARQServiceQueueLo();
+        }
+    }
+    OSRestoreInterrupts(level);
 }
 
-asm void ARQSetChunkSize(register u32 size)
-{
-    nofralloc
-    clrlwi.	r0, r3, 0x1b
-    beq     _80020714
-    subfic	r0, r0, 0x20
-    add	r0, r3, r0
-    stw	r0, lbl_801A6A20
-    blr	
-_80020714:
-    stw	r3, lbl_801A6A20
-    blr	
+// provenance: dolsdk2001:src/ar/arq.c:222 (register-temp form; exact standalone)
+void ARQSetChunkSize(register u32 size) {
+    register u32 m = size & 0x1F;
+    if (m != 0) {
+        lbl_801A6A20 = size + (0x20 - m);
+    } else {
+        lbl_801A6A20 = size;
+    }
 }
 
 // provenance: original
